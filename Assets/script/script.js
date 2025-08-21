5f2ffe459a154fd54c512468bb78b46222e9b60e
Jey Vathsan <jeyvathsan@unimity.com>
2025-08-21 16:05:10 +0530

    Changes in functionality

--- Assets/script/script.js
+++ Assets/script/script.js
@@ -11,3 +11,25 @@ addToCartBtn.addEventListener("click", () => {
   cartCount++;
   cartCountSpan.textContent = cartCount;
 });
+
+
+
+// trial
+
+document.getElementById("filterBtn").addEventListener("click", function () {
+  let checked = Array.from(document.querySelectorAll(".filters input[type=checkbox]:checked"))
+                     .map(cb => cb.value);
+
+  let products = document.querySelectorAll(".product-card");
+
+  products.forEach(product => {
+    let category = product.getAttribute("data-category");
+
+    if (checked.length === 0 || checked.includes(category)) {
+      product.style.display = "flex"; // show
+    } else {
+      product.style.display = "none"; // hide
+    }
+  });
+});
+
